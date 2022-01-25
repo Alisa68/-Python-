4d3bad34e03efbf3699b361c0cec8cb7bf928d8c
Alisa68 Елена <93122395+Alisa68@users.noreply.github.com>
2022-01-25 16:24:25 +0000

    the right decision

--- main.py
+++ main.py
@@ -28,3 +28,49 @@ print("Введите дату: %s" %countries1["Введите дату"])
 print("Введите задачу: %s" %countries1["Введите задачу"])
 print("Введите дату: %s" %countries2["Введите дату"])
 print("Введите задачу: %s" %countries2["Введите задачу"])
+
+
+
+#Решение эталонное
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+​
+s = date + ' ' + task
+print(s)
+​
+# Альтернативное решение
+print(date, task)
+# print может выводить на экран сразу несколько строк через пробел
+
+#Задача №2
+date_1 = input('Введите дату: ')
+task_1 = input('Введите задачу: ')
+date_2 = input('Введите дату: ')
+task_2 = input('Введите задачу: ')
+date_3 = input('Введите дату: ')
+task_3 = input('Введите задачу: ')
+
+print(date_1, task_1)
+print(date_2, task_2)
+print(date_3, task_3)
+
+#Задача №3
+task_dict = {}
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+# Если хотите лучше понять это решение - добавьте вывод на экран после каждого пользовательского ввода
+# print(date, task)
+# print(task_dict)
+# Вы увидите, что переменные date и task изменяются, но данные в словаре task_
